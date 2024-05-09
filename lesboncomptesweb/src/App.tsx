167014ce7b0de1b3dd54bcba9212139134fd1d8c
bilel233 <bilel.mjdb@gmail.com>
2024-05-09 14:55:36 +0200

    Creation de la page connexion

--- lesboncomptesweb/src/App.tsx
+++ lesboncomptesweb/src/App.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import LoginForm from '../src/components/LoginForm';
 
 const App: React.FC = () => {
+
+  const handleLogin = (username: string, password: string) => {
+
+    console.log("Login attempt with:", username, password);
+  };
+
+  const handleLoginWithGoogle = () => {
+
+    console.log("Login with Google requested");
+  };
+
+  const handleLoginWithFacebook = () => {
+
+    console.log("Login with Facebook requested");
+  };
+
   return (
     <ChakraProvider>
-      <div>Hello World</div>
+      {}
+      <LoginForm
+        onLogin={handleLogin}
+        onLoginWithGoogle={handleLoginWithGoogle}
+        onLoginWithFacebook={handleLoginWithFacebook}
+      />
     </ChakraProvider>
   );
 }
