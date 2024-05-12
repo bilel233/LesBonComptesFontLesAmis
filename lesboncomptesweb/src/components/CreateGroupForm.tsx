64ca6cd6e59b9f26c24ea9ae59b66d942406f1dc
bilel233 <bilel.mjdb@gmail.com>
2024-05-12 16:28:01 +0200

    ajout de la responsivite pour le formulaire de creation de groupe

--- lesboncomptesweb/src/components/CreateGroupForm.tsx
+++ lesboncomptesweb/src/components/CreateGroupForm.tsx
@@ -5,7 +5,9 @@ import {
   FormControl,
   FormLabel,
   Input,
-  useToast
+  useToast,
+  VStack,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import axios from 'axios';
 
@@ -22,66 +24,70 @@ const CreateGroupForm: React.FC<CreateGroupFormProps> = ({ onCreate }) => {
   const [groupName, setGroupName] = useState<string>('');
   const toast = useToast();
 
- const handleSubmit = async () => {
-  const token = localStorage.getItem('jwt');
-  if (!token) {
-    toast({
-      title: 'Authentication Error',
-      description: 'No token found, please login again.',
-      status: 'error',
-      duration: 5000,
-      isClosable: true,
-    });
-    return;
-  }
 
-  try {
-    const response = await axios.post('http://localhost:5000/group/create', { name: groupName }, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  const formWidth = useBreakpointValue({ base: "100%", md: "100%" });
 
-    console.log("Response data:", response.data);
-
-    if (response.data && response.data.name && response.data.group_id) {
+  const handleSubmit = async () => {
+    const token = localStorage.getItem('jwt');
+    if (!token) {
       toast({
-        title: 'Group Created',
-        description: `You have created "${response.data.name}".`,
-        status: 'success',
+        title: 'Authentication Error',
+        description: 'No token found, please login again.',
+        status: 'error',
         duration: 5000,
         isClosable: true,
       });
-      onCreate({ id: response.data.group_id, name: response.data.name });
-    } else {
-      throw new Error('Invalid response data');
+      return;
     }
-  } catch (error) {
-    console.error("Error details:", error.response || error);
-    toast({
-      title: 'Failed to create group',
-      description: error.response?.data?.message || "Unexpected error occurred",
-      status: 'error',
-      duration: 5000,
-      isClosable: true,
-    });
-  }
-};
 
+    try {
+      const response = await axios.post('http://localhost:5000/group/create', { name: groupName }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log("Response data:", response.data);
+
+      if (response.data && response.data.name && response.data.group_id) {
+        toast({
+          title: 'Group Created',
+          description: `You have created "${response.data.name}".`,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+        onCreate({ id: response.data.group_id, name: response.data.name });
+      } else {
+        throw new Error('Invalid response data');
+      }
+    } catch (error) {
+      console.error("Error details:", error.response || error);
+      toast({
+        title: 'Failed to create group',
+        description: error.response?.data?.message || "Unexpected error occurred",
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
-    <Box p={5} shadow="md" borderWidth="1px">
-      <FormControl isRequired>
-        <FormLabel>Name of the Group</FormLabel>
-        <Input
-          placeholder="Enter group name"
-          value={groupName}
-          onChange={(e) => setGroupName(e.target.value)}
-        />
-      </FormControl>
-      <Button mt={4} colorScheme="blue" onClick={handleSubmit}>
-        Create Group
-      </Button>
+    <Box p={5} shadow="md" borderWidth="1px" width={formWidth}>
+      <VStack spacing={4}>
+        <FormControl isRequired>
+          <FormLabel>Name of the Group</FormLabel>
+          <Input
+            placeholder="Enter group name"
+            value={groupName}
+            onChange={(e) => setGroupName(e.target.value)}
+          />
+        </FormControl>
+        <Button colorScheme="blue" onClick={handleSubmit}>
+          Create Group
+        </Button>
+      </VStack>
     </Box>
   );
 };
